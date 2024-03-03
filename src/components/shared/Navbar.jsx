56692b8e346ab7fd3c99f66269e5eeebd54e1504
MuhammadDaniyal <daniyalshiekh166@gmail.com>
2024-03-03 23:20:24 +0500

    Initialize folder

---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div className=" h-36">Navbar</div>;
+};
+
+export default Navbar;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroSection from "../../components/Home/HeroSection";
+import Navbar from "../../components/shared/Navbar";
+
+const index = () => {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+    </>
+  );
+};
+
+export default index;
