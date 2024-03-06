ebb2cb04268b81d2a01e59f8e78d15d68568084f
MuhammadDaniyal <daniyalshiekh166@gmail.com>
2024-03-06 19:28:33 +0500

    fixes done

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
 import { FaPlus } from "react-icons/fa6";
 
+const data = [
+  {
+    id: 1,
+    title: "get in touch with us",
+    sublinks: ["Phone: +394304095209", "Support@Company.Com"]
+  },
+  {
+    id: 2,
+    title: "information",
+    sublinks: ["About Us", "Our Products", "Our Products", "Privacy Policy"]
+  },
+  {
+    id: 3,
+    title: "our resources",
+    sublinks: ["Plan And Pricing", "Client Business", "Sales & Purchase", "Product Quality"]
+  },
+]
+
 const Footer = () => {
+  const [isOpen, setIsOpen] = useState([false, false, false]);
+  const [id, setId] = useState();
+  const [links, setLinks] = useState();
+
+
+  const handleOpen = (id) => {
+    let newOpenDropDown = [];
+    newOpenDropDown = isOpen.map((item, i) => {
+      return item.id === id ? !isOpen[id - 1] : false;
+    });
+    setIsOpen(newOpenDropDown);
+  }
+
+  useEffect(() => {
+    const show = data.filter((item) => item.id === id);
+    console.log(show)
+    setLinks(show[0]?.sublinks)
+
+  }, [id])
+
   return (
     <>
       <div className=' flex flex-col gap-5 bg-[#614605] py-10 md:py-4'>
@@ -28,20 +66,29 @@ const Footer = () => {
 
           <div className=' flex flex-col gap-4'>
 
-            <div className=' flex justify-between text-white'>
-              <p className=' capitalize text-[19px]'>get in touch with us</p>
-              <FaPlus className=' text-2xl text-white' />
-            </div>
-
-            <div className=' flex justify-between items-center text-white'>
-              <p className=' capitalize text-[19px]'>information</p>
-              <FaPlus className=' text-2xl text-white' />
-            </div>
-
-            <div className=' flex justify-between text-white'>
-              <p className=' capitalize text-[19px]'>our resources</p>
-              <FaPlus className=' text-2xl text-white' />
-            </div>
+            {
+              data.map((item, i) => (
+                <>
+                  <div className=' flex justify-between text-[#E5E5E5]'>
+                    <p className=' capitalize text-[19px]'>{item.title}</p>
+                    <FaPlus className=' text-2xl text-[#E5E5E5]'
+                      onClick={() => {
+                        setId(item.id)
+                        handleOpen(item.id)
+                      }} />
+                  </div>
+                  {isOpen && links && (item.id === id) && <div>
+                    <ul className=' flex flex-col gap-2 text-[#E5E5E5]'>
+                      {
+                        links && links.map((item, i) => (
+                          <li className=' text-sm'>{item}</li>
+                        ))
+                      }
+                    </ul>
+                  </div>}
+                </>
+              ))
+            }
 
             <div className=' flex justify-between text-white'>
               <p className=' capitalize text-[19px]'>about us</p>
